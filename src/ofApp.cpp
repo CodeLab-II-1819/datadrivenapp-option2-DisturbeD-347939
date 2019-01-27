fcf95ce72484f6ac01d9e9b74b3caeb59b6f5811
DisturbeD-347939 <ricardo.graca17@bathspa.ac.uk>
2019-01-27 18:05:20 +0000

    Added menu and commented get tweet code

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,129 +1,276 @@
-/*
- Starter code for Data Driven App assignment CodeLab II
- Based on basic example of ofxTwitter by Christopher Baker
- <https://christopherbaker.net>
- SPDX-License-Identifier:	MIT
-*/
-
 #include "ofApp.h"
 #include "Internet.h"
 
-//initial app setup
 void ofApp::setup()
 {
-    //lower app frameRate
-    ofSetFrameRate(30);
+	//Initial settings
+	ofSetBackgroundColor(colourBackground);
+	ofSetFrameRate(60);
 
-    // Easily register for all search events.
-    client.registerSearchEvents(this);
+	colourText.r = 29;
+	colourText.g = 161;
+	colourText.b = 242;
 
-    /* Ensure you have created and app and updated the credentials file
-     in the bin/data folder. You can create app at:
-     https://apps.twitter.com */
-    //Load in the credentials for access to twitter
-    client.setCredentialsFromFile("credentials.json");
+	//Twitter settings
+	client.registerSearchEvents(this);
+	client.setCredentialsFromFile("credentials.json");
+	client.setPollingInterval(6000);
+	client.search("hello");
 
-    /* Sets the polling interval for 6 seconds. This means new tweets
-    are retrived every 6 seconds*/
-    client.setPollingInterval(6000);
-
-    // This starts a simple search for an emoticon.
-    client.search(":)");
-	ofxTwitter::SearchQuery query(":)");
-	query.setLanguage("pt");
-	client.search(query);
-    // Tweets are retured in the callbacks onStatus(..), onError(...), etc.
-
-    /*
-     To design more complex searches, see the the API documentation here:
-     https://dev.twitter.com/rest/public/search
-     and:
-     https://developer.twitter.com/en/docs/tweets/rules-and-filtering/overview/standard-operators
-     You can then use the search object ofxTwitter::SearchQuery like this:
-     
-        ofxTwitter::SearchQuery query(":)");
-        query.setLanguage("en");
-        client.search(query);
-
-     This would return only tweets in English
-    
-     To see how else you can refine the queries you should explore
-     the .h files included in:
-     openFrameworks/addons/ofxTwitter/libs/ofxTwitter/include/ofx/Twitter
-     In particular search.h
-    */
-
-	if (internetConnection == NULL)
+	//Check internet connection
+	if (internetConnection == NULL) 
 	{
 		cout << "checking!" << endl;
 		internetConnection = checkInternetConnection();
 		cout << internetConnection << endl;
 	}
+
+	//Load images
+	quit.load("images/quit.png");
 }
 
 void ofApp::update()
 {
-	
+	//Update twitter times
+	for (int i = 0; i < myTweets.size() && i < 15; i++)
+	{
+		myTweets[i].tweetTimer(ofGetElapsedTimeMillis());
+		if (myTweets[i].selfDestruction)
+		{
+			myTweets.erase(myTweets.begin() + i);
+			i--;
+		}
+	}
+	queuedTweets = myTweets.size() - 15;
+
+	//Check if mouse pressed
 }
 
-/*
- Draw function is used to draw to GUI.
- Current setup simply draws tweet count info to a small GUI
- Tweets are displayed in console
-*/
 void ofApp::draw()
 {
-	ofSetBackgroundColor(0);
-	ofSetColor(0, 255, 0);
 	if (!internetConnection)
 	{
+		ofSetBackgroundColor(0);
+		ofSetColor(colourText);
 		ofDrawBitmapString("No internet connection...", ofGetScreenWidth() / 2, ofGetScreenHeight() / 2);
 	}
 	else
 	{
-		ofDrawBitmapString("You're connected!", ofGetScreenWidth() / 2, ofGetScreenHeight() / 2);
+		ofSetColor(colourText);
+		ofDrawBitmapString(xMouse, xMouse + 15, yMouse - 30);
+		ofDrawBitmapString(yMouse, xMouse + 15, yMouse - 20);
+		drawMenu();
+		
+	/*	ofSetColor(0, 255, 0);
+		ofDrawBitmapString(ofGetElapsedTimeMillis(), 10, 10);
+		ofDrawBitmapString(xMouse, xMouse + 15, yMouse - 30);
+		ofDrawBitmapString(yMouse, xMouse + 15, yMouse - 20);
+		ofNoFill();
+		ofDrawBitmapString("Recents tweets", 100, 100);
+		for (int i = 1; i < myTweets.size() + 1 && i < 15; i++)
+		{
+			ofDrawRectangle(100, 120, 200, 50 * i);
+			ofDrawBitmapString(myTweets[i - 1].time, 250, 100 + (50 * i));
+			if (myTweets[i - 1].text.size() > 10)
+			{
+				string firstHalf;
+				string secondHalf;
+				for (int j = 0; j < myTweets[i - 1].text.size(); j++)
+				{
+					if (j <= myTweets[i - 1].text.size() / 2)
+					{
+						firstHalf += myTweets[i - 1].text.at(j);
+						if (j == myTweets[i - 1].text.size() / 2)
+						{
+							if (myTweets[i - 1].text.at(j) != ' ')
+							{
+								if (myTweets[i - 1].text.at(j + 1) != ' ')
+								{
+									firstHalf += '-';
+								}
+							}
+						}
+					}
+					else
+					{
+						secondHalf += myTweets[i - 1].text.at(j);
+					}
+				}
+				ofDrawBitmapString(firstHalf, 150, 90 + (50 * i));
+				ofDrawBitmapString(secondHalf, 150, 110 + (50 * i));
+			}
+			else
+			{
+				ofDrawBitmapString(myTweets[i - 1].text, 150, 100 + (50 * i));
+			}
+		}
+		if (queuedTweets > 0)
+		{
+			int y = 172;
+			ofDrawBitmapString("+", 165, 640);
+			ofDrawBitmapString(queuedTweets, 172, 640);
+			if (queuedTweets <= 9)
+			{
+				ofDrawBitmapString(" tweets queued!", 172 + 10, 640);
+			}
+			else if (queuedTweets >= 10 && queuedTweets <= 99)
+			{
+				ofDrawBitmapString(" tweets queued!", 172 + 12, 640);
+			}
+			else if (queuedTweets >= 100 && queuedTweets <= 999)
+			{
+				ofDrawBitmapString(" tweets queued!", 172 + 14, 640);
+			}
+			else if (queuedTweets >= 1000 && queuedTweets <= 9999)
+			{
+				ofDrawBitmapString(" tweets queued!", 172 + 16, 640);
+			}
+		}*/
 	}
 }
 
 //This function is called everytime a new tweet is received
 void ofApp::onStatus(const ofxTwitter::Status& status)
 {
-    //output the tweet author and text
-    std::cout << "User: " << status.user()->name() << endl;
-    std::cout << "Tweet: " << status.text() << endl;
-	cout << "Fav / Ret: " << status.favoriteCount() << " " << status.retweetCount() << endl;
-	cout << "Source: " << status.source() << endl;
-	cout << "URL: " << status.url() << endl;
-	cout << "Created: " << &Poco::DateTime::dayOfWeek << endl;
-	cout << "Location: " << status.coordinates() << endl;
-    std::cout << "\n -----------------------------\n" << endl;
-    
-    /*
-     To see what other information you can display you should explore
-     the .h files included in:
-     openFrameworks/addons/ofxTwitter/libs/ofxTwitter/include/ofx/Twitter
-     In particular status.h
-    */
+	count++;
+	if (myTweets.size() >= 10)
+	{
+		myTweets.push_back(Tweets(ofGetElapsedTimeMillis(), status.text(), false));
+	}
+	else
+	{
+		myTweets.push_back(Tweets(ofGetElapsedTimeMillis(), status.text(), true));
+	}
+	//ofLogNotice("ofApp::onStatus") << "Text: " << status.text();
+	//ofLogNotice("ofApp::onStatus") << "\tPlace: " << (status.place() ? ofToString(status.place()->fullName()) : "NONE");
 }
 
-//returns an error message if error encountered recieving tweets
+//DrawMenu
+void ofApp::drawMenu()
+{
+	ofSetColor(colourText);
+	font.loadFont("HelveticaNeueUltraLight.ttf", 40);
+	ofNoFill();
+	ofDrawRectangle(1, 1, 1500, 60);
+
+	//Home tab
+	ofRectangle Home(1, 1, 175, 60);
+	if (Home.inside(xMouse, yMouse)) //Change colour on mouse hover
+	{
+		ofFill();
+		ofDrawRectangle(Home);
+		ofSetColor(colourBackground);
+		font.drawString("Home", 20, 50);
+		ofSetColor(colourText);
+	}
+	else
+	{
+		ofNoFill();
+		ofDrawRectangle(Home);
+		ofSetColor(colourText);
+		font.drawString("Home", 20, 50);
+	}
+
+	//Search tab
+	ofRectangle Search(176, 1, 200, 60);
+	if (Search.inside(xMouse, yMouse)) //Change colour on mouse hover
+	{
+		ofFill();
+		ofDrawRectangle(Search);
+		ofSetColor(colourBackground);
+		font.drawString("Search", 200, 50);
+		ofSetColor(colourText);
+	}
+	else
+	{
+		ofNoFill();
+		ofDrawRectangle(Search);
+		ofSetColor(colourText);
+		font.drawString("Search", 200, 50);
+	}
+
+	//Settings tab
+	ofRectangle Settings(376, 1, 220, 60);
+	if (Settings.inside(xMouse, yMouse)) //Change colour on mouse hover
+	{
+		ofFill();
+		ofDrawRectangle(Settings);
+		ofSetColor(colourBackground);
+		font.drawString("Settings", 400, 50);
+		ofSetColor(colourText);
+	}
+	else
+	{
+		ofNoFill();
+		ofDrawRectangle(Settings);
+		ofSetColor(colourText);
+		font.drawString("Settings", 400, 50);
+	}
+
+	//Quit tab
+	ofRectangle Quit(1420, 1, 79, 60);
+	if (Quit.inside(xMouse, yMouse)) //Change colour on mouse hover
+	{
+		ofFill();
+		ofDrawRectangle(Quit);
+		ofSetColor(colourBackground);
+		quit.draw(1440, 11, 39, 40);
+		ofSetColor(colourText);
+	}
+	else
+	{
+		ofNoFill();
+		ofDrawRectangle(Quit);
+		ofSetColor(colourText);
+		quit.draw(1440, 11, 39, 40);
+	}
+
+	if (Home.inside(xMouseClick, yMouseClick))
+	{
+		cout << "Inside home" << endl;
+	}
+}
+
 void ofApp::onError(const ofxTwitter::Error& error)
 {
-    std::cout << "Error: " << error.code() << " " << error.message();
-}
 
-//returns an exception message if exception encountered recieving tweets
-void ofApp::onException(const std::exception& notice)
-{
-    std::cout << "Exception: " << notice.what();
 }
-
-void ofApp::onMessage(const ofJson& json)
-{
-	// This is the raw message json and is ignored here.
-}
-
-void ofApp::varSetup()
+void ofApp::onException(const std::exception& exception)
 {
 
 }
+
+void ofApp::mousePressed(int x, int y)
+{
+	xMouseClick = x;
+	yMouseClick = y;
+}
+void ofApp::mouseMoved(int x, int y)
+{
+	xMouse = x;
+	yMouse = y;
+}
+
+Tweets::Tweets(double time, string tweet, bool startCountdown)
+{
+	this->startCountdown = startCountdown;
+	this->text = tweet;
+	this->time = time;
+}
+
+void Tweets::tweetTimer(double current_time)
+{
+	if (startCountdown)
+	{
+		if ((current_time - time) > interval)
+		{
+			selfDestruction = true;
+		}
+	}
+	else
+	{
+		time = current_time;
+		startCountdown = true;
+	}
+}
+
--- src/ofApp.h
+++ src/ofApp.h
@@ -12,6 +12,18 @@
 #include "ofMain.h"
 #include "ofxTwitter.h"
 
+class Tweets
+{
+public:
+	string text;
+	double interval = 5000;
+	double time;
+	bool selfDestruction = false;
+	bool startCountdown = false;
+	void tweetTimer(double time);
+
+	Tweets(double current_time, string tweet, bool startCountdown);
+};
 
 class ofApp: public ofBaseApp
 {
@@ -25,8 +37,22 @@ public:
     void onException(const std::exception& exception);
 	void onMessage(const ofJson& json);
 	void mouseMoved(int x, int y);
+	void mousePressed(int x, int y);
 
-	void varSetup();
+	//Functions
+	void drawMenu();
+
+	//Images
+	ofImage quit;
+
+	int queuedTweets;
+	int count = 0;
+
+	//Mouse
+	int xMouse;
+	int yMouse;
+	int xMouseClick = 0;
+	int yMouseClick = 0;
     
 	//declare twitter search client
     ofxTwitter::SearchClient client;
@@ -34,5 +60,15 @@ public:
     //declare variables
 	bool internetConnection;
 	bool reset = false;
+
+	//Colour defaults
+	ofColor colourText;
+	ofColor colourBackground = (255, 255, 255);
+
+	//For fonts and sizes
+	ofTrueTypeFont font;
+
+	//Tweet vector
+	vector<Tweets> myTweets;
 
 };
